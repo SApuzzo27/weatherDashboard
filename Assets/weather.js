9471b244e4d8f43e1044152a207e4d8b2327b104
Sam <SApuzzo527@gmail.com>
2020-09-13 23:02:09 -0400

    added queryurl and api key

---
+++ Assets/weather.js
@@ -0,0 +1,13 @@
+// pulls information from the form and build the query URL
+// var apiKey = "188366c4a74fbb8bc6a6f7d868a6bfa2"
+
+// function buildQueryURL() {
+//         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
+//         cityName + "&appid=” + apiKey; 
+
+
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function(response);
+// });
